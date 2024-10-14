18456d2eba584c08ecabb2456a25105be2c55f19
fuky <123cr0002@tokyo.amg.ac.jp>
2024-10-14 16:27:50 +0900

    球描画用コンポーネント追加

---
+++ AppFrame/source/System/Header/Component/DrawSphereComponent.h
@@ -0,0 +1,20 @@
+//----------------------------------------------------------------------
+// @filename DrawComponent.h
+// @author: Fukuma Kyohei
+// @explanation
+// 球の描画用コンポーネントクラス
+//----------------------------------------------------------------------
+#pragma once
+#include "DrawComponent.h"
+class DrawSphereComponent : public DrawComponent
+{
+public:
+	DrawSphereComponent(class ObjectBase* owner, int updateowder);
+	virtual ~DrawSphereComponent();
+	void Update()override;
+	//セッター
+	void SetSphere(class Sphere* sphere) { m_Sphere = sphere; }
+protected:
+	class Sphere* m_Sphere;
+};
+
